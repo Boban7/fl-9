7ea4bfbe29b14f979052322786a30086dd435991
NazarBodak <bodaknazar@gmail.com>
2018-08-17 23:05:44 +0300

    hw #6 - add parseFloat to myRound()

--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -7,11 +7,11 @@ function calculation(s1, s2, angDeg) {
 	const deg180 = 		180;
 	const angRad = 		angDeg * Math.PI / deg180;
 	const s3 = 			myRound(Math.sqrt(s1 * s1 + s2 * s2 - 2 * s1 * s2 * Math.cos(angRad)));
-	const perimeter = 	myRound(s1 + s2 + s3);
+	const perimeter = 	myRound(s3 + s1 + s2);
 	const square = 		myRound(s1 * s2 / 2 * Math.sin(angRad));
 
 	function myRound(x) {
-		return x % 1 ? (Math.round(x * 100)/100).toFixed(2) : x;
+		return x % 1 ? parseFloat((Math.round(x * 100)/100).toFixed(2)) : x;
 	}
 	
 	if(s3 > 0 && perimeter > 0 && square > 0) {
