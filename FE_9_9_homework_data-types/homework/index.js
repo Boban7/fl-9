868a56996116a4e62ded7b3378da5a9831953d3c
Nazar Bodak <32780092+Boban7@users.noreply.github.com>
2018-08-27 08:40:18 +0300

    Update index.js
    task 8

--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -1,39 +1,4 @@
-// Your code goes here
+/*8. Write function, which returns formatted date.
 
-
-/*
-[
-  {
-    "_id": "5b5e3168c6bf40f2c1235cd6",
-    "index": 0,
-    "age": 39,
-    "eyeColor": "green",
-    "name": "Stein",
-    "favoriteFruit": "apple"
-  },
-  {
-    "_id": "5b5e3168e328c0d72e4f27d8",
-    "index": 1,
-    "age": 38,
-    "eyeColor": "blue",
-    "name": "Cortez",
-    "favoriteFruit": "strawberry"
-  },
-  {
-    "_id": "5b5e3168cc79132b631c666a",
-    "index": 2,
-    "age": 2,
-    "eyeColor": "blue",
-    "name": "Suzette",
-    "favoriteFruit": "apple"
-  },
-  {
-    "_id": "5b5e31682093adcc6cd0dde5",
-    "index": 3,
-    "age": 17,
-    "eyeColor": "green",
-    "name": "Weiss",
-    "favoriteFruit": "banana"
-  }
-]
-*/
+showFormattedDate(new Date('2018-08-27T01:10:00')) // returns ‘It is 27 of Aug, 2018’
+// every month should be showed as 3 letters (e.g. Feb, Apr or Dec)*/
