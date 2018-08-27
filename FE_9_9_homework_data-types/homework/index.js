77bc3e264c4948a7b3d2d70581ac57ff23ef37b4
Nazar Bodak <32780092+Boban7@users.noreply.github.com>
2018-08-27 09:41:20 +0300

    Update index.js
    task 8.8

--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -2,3 +2,26 @@
 
 showFormattedDate(new Date('2018-08-27T01:10:00')) // returns ‘It is 27 of Aug, 2018’
 // every month should be showed as 3 letters (e.g. Feb, Apr or Dec)*/
+
+function showFormattedDate(x) {
+	let monthLength3Letter = function(date) {
+		let month = [
+		    "January",
+		    "February",
+		    "March",
+		    "April",
+		    "May",
+		    "June",
+		    "July",
+		    "August",
+		    "September",
+		    "October",
+		    "November",
+		    "December"
+		]
+		return month[date.getMonth()].slice(0,3);
+	}
+   	return `It is ${x.getDate()} of ${monthLength3Letter(x)}, ${x.getFullYear()}`;
+}
+
+alert(showFormattedDate(new Date('2018-08-27T01:10:00')));
