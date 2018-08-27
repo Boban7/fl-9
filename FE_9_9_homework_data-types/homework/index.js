9eaec61e9ec6c09f8e873eb222e7ecbd4870bbfc
NazarBodak <bodaknazar@gmail.com>
2018-08-28 00:13:32 +0300

    hw #9 - ready2

--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function findType(param) {
   return typeof param;
 }
@@ -90,32 +89,3 @@ function showFormattedDate(x) {
   }
     return `It is ${x.getDate()} of ${monthLength3Letter(x)}, ${x.getFullYear()}`;
 }
-=======
-/*8. Write function, which returns formatted date.
-
-showFormattedDate(new Date('2018-08-27T01:10:00')) // returns ‘It is 27 of Aug, 2018’
-// every month should be showed as 3 letters (e.g. Feb, Apr or Dec)*/
-
-function showFormattedDate(x) {
-	let monthLength3Letter = function(date) {
-		let month = [
-		    "January",
-		    "February",
-		    "March",
-		    "April",
-		    "May",
-		    "June",
-		    "July",
-		    "August",
-		    "September",
-		    "October",
-		    "November",
-		    "December"
-		]
-		return month[date.getMonth()].slice(0,3);
-	}
-   	return `It is ${x.getDate()} of ${monthLength3Letter(x)}, ${x.getFullYear()}`;
-}
-
-alert(showFormattedDate(new Date('2018-08-27T01:10:00')));
->>>>>>> 77bc3e264c4948a7b3d2d70581ac57ff23ef37b4
